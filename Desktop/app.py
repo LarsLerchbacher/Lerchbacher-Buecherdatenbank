cf57e4765e70eeb2eabc8c8167c7e8fdfe44631f
Lars Lerchbacher <lars.lerchbacher@aon.at>
2025-08-23 12:15:13 +0200

    Made licensing for the project clear
    The project is now licensed under the GNU AGPL v3
    I added a license disclaimer to every file, that did not include
    unfinished changes (can't commit those yet)

--- Desktop/app.py
+++ Desktop/app.py
@@ -1,6 +1,19 @@
 #
-# Lerchbacher book database desktop
-# Copyright 2025 Lars Lerchbacher
+# Desktop/app.py
+# ----------
+#
+#   The Lerchbacher book database project
+#   © Lars Lerchbacher 2025
+#
+#   This file is part of the Lerchbacher book database
+#
+#   The Lerchbacher book database is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation,
+#   either version 3 of the License, or (at your option) any later version.
+#
+#   The Lerchbacher book database is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License along with the Lerchabcher book database. If not, see <https://www.gnu.org/licenses/>. 
 #
 
 
